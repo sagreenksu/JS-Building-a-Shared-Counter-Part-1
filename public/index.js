4b8a5462603b1b1fd885ce498a92deaf37be734f
Scott Green <sagreenxyz@gmail.com>
2022-01-24 15:53:10 -0600

    With Bonus Functionality

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button');
 
 
     let respJSON = await fetch('http://localhost:9001/counter');
@@ -18,8 +19,16 @@ async function main(){
         countContainer.textContent = countValue;
     }
 
+    async function refresh(){
+        let respJSON = await fetch('http://localhost:9001/counter');
+        let resp = await respJSON.json();
+        countValue = resp.value
+        countContainer.textContent = countValue;
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh);
     countContainer.textContent = countValue;
 }
 main()
